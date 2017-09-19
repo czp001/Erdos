449613914a26a5d51e65d331173e3ab1e2f48db6
czp001 <30425821+czp001@users.noreply.github.com>
2017-09-19 10:45:43 +0800

    Update er047.cpp

--- er047.cpp
+++ er047.cpp
@@ -1,57 +1,44 @@
-#include <iostream>
 #include <cstdio>
 #include <cstring>
 #include <algorithm>
-#include <map>
 #include <ctime>
 using namespace std;
+typedef long long ll;
+const int maxn = 10000000;
+bool is_prime[maxn];
+bool is_prime_small[maxn];
+ll prime[maxn];
+ll prime_num=0;
 
-typedef long long LL;
+//对区间[a,b)内的整数执行筛法，is_prime[i-a]=true  ---  表示i是素数 注意这里下标偏移了a，所以从0开始。
+ll segment_sieve(ll a,ll b) {
+	prime_num=0;
+    for(ll i=0;i*i<b;++i) is_prime_small[i]=true; //对[2,sqrt(b))的初始化全为质数
+    for(ll i=0;i<b-a;++i) is_prime[i]=true; //对下标偏移后的[a,b)进行初始化
 
-const int maxn = 200000;
-const int MOD = 2333333;
-const int maxp = 131100;
-
-long long pcnt, prime[maxp];
-short mu[maxn];
-bool vis[maxn];
-
-void preprocess() {
-	mu[1] = 1;
-	for(int i = 2; i < maxn; i++) {
-		if(!vis[i]) {
-			mu[i] = -1;
-			prime[pcnt++] = i;
-		}
-		for(int j = 0; j < pcnt && i * prime[j] < maxn; j++) {
-			vis[i * prime[j]] = true;
-			if(i % prime[j] != 0) mu[i * prime[j]] = -mu[i];
-			else {
-				mu[i * prime[j]] = 0;
-				break;
-			}
-		}
-	}
-
-	for(int i = 2; i < maxn; i++) mu[i] += mu[i-1];
+    for(ll i=2;i*i<b;++i) {
+        if(is_prime_small[i]) {
+            for(ll j=2*i;j*j<b;j+=i) is_prime_small[j]=false;  //筛选[2,sqrt(b));
+            //(a+i-1)/i得到最接近a的i的倍数，最低是i的2倍，然后筛选
+            for(ll j=max(2LL,(a+i-1)/i)*i;j<b;j+=i) is_prime[j-a]=false;
+        }
+    }
+    for(ll i=0;i<b-a;++i)  //统计个数
+        if(is_prime[i]&&(i%4==3)) prime[prime_num++]=i+a;
+	return prime_num;
 }
-
 
 int main()
 {
 	clock_t start=clock();
-	preprocess();
-	int ans=0;
-	for(long long i=2147483648;i<=4294967295;i++)
-	{
-		int flag=1;
-		for(int j=0;j<=12250;j++)
-		{
-			if((4*i+3)%prime[j]==0){flag=0;break;}
-		}
-		ans+=flag;
-	}	
-	printf("%d\n",ans);
+    ll a,b,ans=0;
+    for(ll i=1;i<=858;i++)
+    {
+        memset(prime,0,sizeof(prime));
+        ans+=segment_sieve(8589934592+(i-1)*10000000,8589934592+i*10000000);
+    }
+	ans+=segment_sieve(17169934592,17179869184);
+	printf("%lld\n",ans);
 	printf("%f\n",(clock()-start)/1000000.);
-	return 0;
+    return 0;
 }
