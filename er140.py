dd4d5a0dee0db04c15819aaab261fd569da497f5
czp001 <30425821+czp001@users.noreply.github.com>
2017-09-25 14:47:53 +0800

    Update er140.py

--- er140.py
+++ er140.py
@@ -28,11 +28,11 @@ def pi(n,k):
                     S[v] -= p**k*(S[v//p] - sp)
         return S[n]
     else:
-	i,ans=1,0
-	while prime[i]**k<=n:
-		ans+=prime[i]**k
+	    i,ans=1,0
+	    while prime[i]**k<=n:
+		    ans+=prime[i]**k
 			if 2*prime[i]**k<=n:
-				ans+=2*prime[i]**k
+                ans+=2*prime[i]**k
 			i+=1
 	return ans
         
