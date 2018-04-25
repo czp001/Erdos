7b49e5fbe1a3062320a61b539964d4fa0fa9d639
czp001 <30425821+czp001@users.noreply.github.com>
2018-04-25 16:30:24 +0800

    Update er138.cpp

--- er138.cpp
+++ er138.cpp
@@ -4,10 +4,11 @@
 
 using int64 = long long;
 using int128 = __int128;
+const int64 N=1e11;
 
 int64 sd(int64 n)
 {
-	int128 sum=0,i,sq=floor(sqrt(n));
+	int64 sum=0,i,sq=floor(sqrt(n));
 	for(i=1;i<=sq;i++)
 	{
 		sum+=n/i;
@@ -17,7 +18,7 @@ int64 sd(int64 n)
 
 int64 k3(int64 n)
 {
-	int128 x=pow(10,log10(n)/2);
+	int64 x=pow(10,log10(n)/2);
 	while(!(x*x*x<=n&&(x+1)*(x+1)*(x+1)>n))
 	{
 		x=(2*x+n/(x*x))/3;
@@ -55,10 +56,8 @@ int64 f(int64 n)
 int main()
 {
 	//const int64 base = 1000000000000000000ll;
-	int64 n=1000000000000ll;
 	clock_t start=clock();
-	printf("%lld\n",n);
-   	printf("%lld\n",f(n));
+   	printf("%lld\n",f(N));
 	//printf("%lld%018lld\n", int64(f(n) / base), int64(f(n) % base));
 	printf("%f\n",(clock()-start)/1000000.);
 	return 0;
